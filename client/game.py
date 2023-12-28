c39212940925a43f0e5de73865d682059fdd6d01
Times0 <45049767+Times0@users.noreply.github.com>
2023-12-28 16:38:44 +0100

    added basic menu

--- client/game.py
+++ client/game.py
@@ -1,21 +1,25 @@
 import os
 import threading
-from PygameUIKit import Group
+from typing import Optional
+
+from PygameUIKit import Group, button
+from PygameUIKit.label import Label
 from PygameUIKit.utilis import load_image
+from pygame import Color
 
 from board_ui import Board, get_x_y_w_h, pygame
-from logic import Logic, Color, State
+from logic import Logic, State
 from PygameUIKit.button import ButtonPngIcon
 from reseau import *
 import time
 import logging
-
 
 logging.basicConfig(level=logging.INFO)
 
 BACKGROUND_COLOR = (22, 21, 18)
 COLOR_CHANGING = (0, 85, 170)
 
+FONT = pygame.font.SysFont("lucidafaxdemigras", 30)
 SERVER_MODE = False
 
 
@@ -29,6 +33,10 @@ class Game:
         self.game_on = True
         self.window_on = True
 
+        # Menus
+        self.menu = Menu()
+        self.menu.open("game_selection")
+
         # Server
         self.socket = None
         self.server_thread = None
@@ -40,6 +48,7 @@ class Game:
         # button to flip the board
         img_flip = load_image(os.path.join("assets", "other", "flip.png"), (25, 25))
         self.ui = Group()
+        self.ui.add(self.menu)
         self.btn_flip_board = ButtonPngIcon(img_flip, self.flip_board, ui_group=self.ui)
 
     def run(self):
@@ -66,6 +75,14 @@ class Game:
             if event.type == pygame.WINDOWRESIZED:
                 self.board.rect = pygame.Rect(get_x_y_w_h())
 
+    def draw(self):
+        x, y, w, h = get_x_y_w_h()
+        self.win.fill(BACKGROUND_COLOR)
+        self.board.draw(self.win)
+        self.btn_flip_board.draw(self.win, x + w - 25, y + h + 10)
+        self.menu.draw(self.win)
+        pygame.display.flip()
+
     def play(self, move):
         if SERVER_MODE:
             logging.info(f"Sending move: {move.get_uci()} to server")
@@ -78,13 +95,6 @@ class Game:
     def check_end(self):
         if self.logic.state != State.GAMEON:
             self.game_on = False
-
-    def draw(self):
-        x, y, w, h = get_x_y_w_h()
-        self.win.fill(BACKGROUND_COLOR)
-        self.board.draw(self.win)
-        self.btn_flip_board.draw(self.win, x + w - 25, y + h + 10)
-        pygame.display.flip()
 
     def update_board_if_new_info(self):
         old_fen = self.logic.get_fen()
@@ -194,3 +204,78 @@ class Game:
 
     def flip_board(self):
         self.board.flipped = not self.board.flipped
+
+
+class MenuPart:
+    def __init__(self):
+        self._name: str = ""
+        self.rect: pygame.Rect = pygame.Rect(0, 0, 0, 0)
+        self.name_label: Optional[Label] = None
+        self.ui = Group()
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if self._name != value:
+            self._name = value
+            self.name_label = Label(self._name.title().replace("_",' '), font_color=Color("black"), font=FONT)
+            self.name_label.rect.midtop = self.rect.midtop
+
+    def handle_event(self, event):
+        self.ui.handle_event(event)
+
+    def draw(self, win):
+        self.rect.center = win.get_rect().center
+        pygame.draw.rect(win, Color("gray"), self.rect)
+
+        if self.name_label:
+            self.name_label.draw(win, *self.rect.move(10, 10).topleft)
+
+
+class GameModeSelection(MenuPart):
+    def __init__(self):
+        super().__init__()
+        self.name = "mode_selection"
+        self.rect = pygame.Rect(0, 0, 300, 300)
+
+        params = {"rect_color": (199, 201, 207),
+                  "font": FONT,
+                  "fixed_width": self.rect.w - 20,
+                  "text_align": "center",
+                  "ui_group": self.ui}
+        self.btn_local = button.ButtonText("1v1 Local", **params)
+        self.btn_online = button.ButtonText("1v1 Online", **params)
+        self.btn_bot = button.ButtonText("1vBot", **params)
+
+    def draw(self, win):
+        super().draw(win)
+        self.btn_local.draw(win, *self.rect.move(10, 75).topleft)
+        self.btn_online.draw(win, *self.rect.move(10, 150).topleft)
+        self.btn_bot.draw(win, *self.rect.move(10, 225).topleft)
+
+
+class Menu:
+    def __init__(self):
+        self.menus: dict[str, MenuPart] = {"game_selection": GameModeSelection()}
+        self.active_element: Optional[MenuPart] = None
+        self.rect = pygame.display.get_surface().get_rect()
+
+    def add_elements(self, elements: list[MenuPart]):
+        if elements is None:
+            return
+        for element in elements:
+            self.menus[element.name] = element
+
+    def open(self, name):
+        self.active_element = self.menus[name]
+
+    def draw(self, win):
+        if self.active_element:
+            self.active_element.draw(win)
+
+    def handle_event(self, event):
+        if self.active_element:
+            self.active_element.handle_event(event)
